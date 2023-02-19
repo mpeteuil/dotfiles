4a1295b130e5f689b562a40bd21201618098fc95
Michael Peteuil <michael.peteuil@gmail.com>
2023-02-19 16:39:29 -0500

    shrc: tweak Homebrew settings.
    https://github.com/MikeMcQuaid/dotfiles/commit/7dab9ef24f5f0855febe15bd9a25893af32739b4

--- shrc.sh
+++ shrc.sh
@@ -121,13 +121,13 @@ if quiet_which brew
 then
   eval $(brew shellenv)
 
-  export HOMEBREW_AUTO_UPDATE_SECS=3600
   export HOMEBREW_DEVELOPER=1
   export HOMEBREW_BUNDLE_BREW_SKIP=""
   export HOMEBREW_BUNDLE_CASK_SKIP=""
   # export HOMEBREW_NO_ENV_HINTS=1
   export HOMEBREW_INSTALL_FROM_API=1
   export HOMEBREW_AUTOREMOVE=1
+  export HOMEBREW_ANALYTICS_ENABLE_INFLUX=1
 
   add_to_path_end "$HOMEBREW_PREFIX/Library/Homebrew/shims/gems"
 
