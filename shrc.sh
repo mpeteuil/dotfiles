420c4bdce2f09deb58c19aebcb14853c76cebc1a
Michael Peteuil <michael.peteuil@gmail.com>
2023-02-19 13:43:31 -0500

    shrc: tweak cross-platform settings.
    https://github.com/MikeMcQuaid/dotfiles/commit/fa29ed4f2ecb6ee7b1a9552a0cb2a7f5bfd9372c

--- shrc.sh
+++ shrc.sh
@@ -110,7 +110,7 @@ alias gcpa="git cherry-pick --abort"
 alias gcpc="git cherry-pick --continue"
 alias grlb="git branch --merged | egrep -v '(^\*|master)' | xargs git branch -d"
 
-# Platform-specific stuff
+# Command-specific stuff
 if quiet_which brew
 then
   export HOMEBREW_PREFIX="$(brew --prefix)"
@@ -126,44 +126,50 @@ then
   alias hbc='cd $HOMEBREW_REPOSITORY/Library/Taps/homebrew/homebrew-core'
 fi
 
+if quiet_which diff-so-fancy
+then
+  # shellcheck disable=SC2016
+  export GIT_PAGER='diff-so-fancy | less -+$LESS -RX'
+else
+  # shellcheck disable=SC2016
+  export GIT_PAGER='less -+$LESS -RX'
+fi
+
+if quiet_which exa
+then
+  alias ls="exa --classify --group --git"
+elif [ "$MACOS"]
+then
+  alias ls="ls -F"
+else
+  alias ls="ls -F --color=auto"
+fi
+
+if quiet_which bat
+then
+  export BAT_THEME="ansi"
+  alias cat="bat"
+fi
+
+if quiet_which htop
+then
+  alias top="sudo htop"
+fi
+
+# Configure environment
+export GREP_OPTIONS="--color=auto"
+export CLICOLOR=1
+# export RESQUE_REDIS_URL="redis://localhost:6379"
+export GITHUB_USE_HOMEBREW_BINARIES=1
+export HOMEBREW_GITHUB_USE_HOMEBREW_BINARIES=1
+export GITHUB_NO_AUTO_BOOTSTRAP=1
+export BOOTSTRAP_DISABLE_ISSUES=1
+export GITHUB_PROFILE_BOOTSTRAP=1
+
+# OS-specific configuration
 if [ "$MACOS" ]
 then
-  export GREP_OPTIONS="--color=auto"
-  export CLICOLOR=1
   export VAGRANT_DEFAULT_PROVIDER="vmware_fusion"
-  # export RESQUE_REDIS_URL="redis://localhost:6379"
-  export GITHUB_USE_HOMEBREW_BINARIES=1
-  export HOMEBREW_GITHUB_USE_HOMEBREW_BINARIES=1
-  export GITHUB_NO_AUTO_BOOTSTRAP=1
-  export BOOTSTRAP_DISABLE_ISSUES=1
-  export GITHUB_PROFILE_BOOTSTRAP=1
-
-  if quiet_which diff-so-fancy
-  then
-    # shellcheck disable=SC2016
-    export GIT_PAGER='diff-so-fancy | less -+$LESS -RX'
-  else
-    # shellcheck disable=SC2016
-    export GIT_PAGER='less -+$LESS -RX'
-  fi
-
-  if quiet_which exa
-  then
-    alias ls="exa --classify --group --git"
-  else
-    alias ls="ls -F"
-  fi
-
-  if quiet_which bat
-  then
-    export BAT_THEME="ansi"
-    alias cat="bat"
-  fi
-
-  if quiet_which htop
-  then
-    alias top="sudo htop"
-  fi
 
   add_to_path_end "$HOMEBREW_PREFIX/opt/git/share/git-core/contrib/diff-highlight"
   add_to_path_end "/Applications/Fork.app/Contents/Resources"
@@ -177,6 +183,7 @@ then
   alias locate="mdfind -name"
   alias finder-hide="setfile -a V"
 
+  # make no-argument find Just Work.
   find() {
     local arg
     local path_arg
@@ -197,12 +204,9 @@ then
   quiet_which keychain && eval "$(keychain -q --eval --agents ssh id_rsa)"
 
   alias su="/bin/su -"
-  alias ls="ls -F --color=auto"
   alias open="xdg-open"
 elif [ "$WINDOWS" ]
 then
-  alias ls="ls -F --color=auto"
-
   open() {
     # shellcheck disable=SC2145
     cmd /C"$@"
@@ -220,7 +224,6 @@ elif quiet_which vi
 then
   export EDITOR="vi"
 fi
-alias e="$EDITOR"
 
 # Set up version control editors specifically
 if quiet_which nvim
