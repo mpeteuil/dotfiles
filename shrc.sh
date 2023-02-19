545ec66ac1a50bfb75782b8d31ae0eee4dae2134
Michael Peteuil <michael.peteuil@gmail.com>
2023-02-19 16:23:22 -0500

    shrc: add more helpers.
    https://github.com/MikeMcQuaid/dotfiles/commit/418f743005056a45c333b2e2debdc4c88ee05420

--- shrc.sh
+++ shrc.sh
@@ -219,6 +219,11 @@ then
   export JEKYLL_GITHUB_TOKEN="$GITHUB_TOKEN"
   export BUNDLE_RUBYGEMS__PKG__GITHUB__COM="$GITHUB_TOKEN"
 
+  # output what's listening on the supplied port
+  on-port() {
+    sudo lsof -nP -i4TCP:$1
+  }
+
   # make no-argument find Just Work.
   find() {
     local arg
@@ -318,6 +323,11 @@ trash() {
 # GitHub API shortcut
 github-api-curl() {
   noglob curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/$1"
+}
+
+# Spit out Okta keychain password
+okta-keychain() {
+  security find-generic-password -l device_trust '-w'
 }
 
 # Look in ./bin but do it last to avoid weird `which` results.
