e496ef699a9bb5da95fe1fab2d410ccf4da203a2
Michael Peteuil <michael.peteuil@gmail.com>
2026-03-05 21:31:57 -0500

    Use duf and dust
    https://github.com/MikeMcQuaid/dotfiles/commit/826ce63368d175f9350449243816a1038cfb35bd

--- shrc.sh
+++ shrc.sh
@@ -161,6 +161,14 @@ if quiet_which bat; then
   export HOMEBREW_BAT=1
 fi
 
+if quiet_which dust; then
+  alias du="dust"
+fi
+
+if quiet_which duf; then
+  alias df="duf"
+fi
+
 if quiet_which htop; then
   alias top="sudo htop"
 fi
